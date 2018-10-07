2781980f8d8efdbc45be1532d41964eed608e9a8
Brianna Besinaiz <blbesinaiz@gmail.com>
2018-10-07 10:29:52 -0500

    Add files via upload

---
+++ nytAPI.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class nytAPI
+{
+   public static void main(String[] args)
+   {
+      String API = "003d6acd99504f0ea277f0a22b1ca35b";
+      System.out.println("New York Times API Key: " + API);
+   }
+}
